621b5b906eebb3ed79d6901d23d8d89f620d9e27
Sangeeta Maharana <sangeetamaharana246@gmail.com>
2026-02-14 21:53:41 +0530

    IsArray in Javascript

---
+++ Javascript/Module_5/IsArray.js
@@ -0,0 +1,12 @@
+// Array.IsArray() -> This method helps to identify if the type is an array or not
+// Returns a boolean value
+
+let arr = [1,2,3,4]
+let checkArr = Array.isArray(arr);
+console.log(checkArr);
+// O/P -> true
+
+let a = 2;
+let checkArray = Array.isArray(a);
+console.log(checkArray);
+// O/P --> false
