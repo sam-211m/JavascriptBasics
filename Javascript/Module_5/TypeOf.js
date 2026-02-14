09e0d8c1264d8b587bda2439088261b683549733
Sangeeta Maharana <sangeetamaharana246@gmail.com>
2026-02-14 21:40:06 +0530

    TypeOf in Javascript

---
+++ Javascript/Module_5/TypeOf.js
@@ -0,0 +1,22 @@
+// TypeOf
+// Returns the type of data 
+
+
+let a = 10;
+console.log(typeof(a))
+// O/P -> number
+
+
+let name = "sam"
+console.log(typeof(name));
+// O/P -> string
+
+let obj = {
+    day:"Wednesday"
+}
+console.log(typeof(obj))
+// O/P --> object
+
+let arr = [1,2,3,4]
+console.log(typeof(arr));
+// O/P --> object
